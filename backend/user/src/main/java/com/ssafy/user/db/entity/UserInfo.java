ca52d55f4fce5e8866e07eef902bc3299d9949a7
이방환 <soulb9@naver.com>
2023-03-17 10:30:53 +0900

    feature: UserInfo 엔티티와 리포지토리 추가
    ERD 초안에 기반한 엔티티 설계를 진행했습니다.
    - db/entity/UserInfo.java
    - db/repository/UserinfoRepository.java
    
    Issue:

--- backend/user/src/main/java/com/ssafy/user/db/entity/UserInfo.java
+++ backend/user/src/main/java/com/ssafy/user/db/entity/UserInfo.java
@@ -5,24 +5,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Immutable;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.annotations.ListIndexJdbcTypeCode;
-import org.hibernate.type.SqlTypes;
 
 
-@Table(name = "user_info")
-@NoArgsConstructor
-@Getter
+@Data
 @Entity
 public class UserInfo {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  @Column(nullable = false, unique = true)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+//  @Column(nullable = false, unique = true)
   private Integer userId;
 
   @Column(nullable = false)
--- backend/user/src/main/java/com/ssafy/user/db/repository/UserinfoRepository.java
+++ backend/user/src/main/java/com/ssafy/user/db/repository/UserinfoRepository.java
@@ -1,9 +1,7 @@
 package com.ssafy.user.db.repository;
 
 import com.ssafy.user.db.entity.UserInfo;
-import jakarta.persistence.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserinfoRepository extends JpaRepository<UserInfo, Integer> {
-
 }
