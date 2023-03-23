e49e6892f66b3a54d14ea5dce1bf4d6961414ba1
이석훈 <cqe9933@naver.com>
2023-03-23 11:12:19 +0900

    🎨 feat: 로그인 페이지 구현

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import loginSpeechBackground from "../../assets/image/loginSpeechBackground.png";
-import KakaoLogin from "../../assets/icon/kakaoLogin.svg";
+import KakaoLogin from "../../assets/icon/kakaoLogin.png";
 import * as L from "./LoginStyle";
+
 /**로그인 컴포넌트 */
 const Login = () => {
   const [height, setHeight] = useState(0);
@@ -22,6 +23,8 @@ const Login = () => {
     <L.LoginBackBackground>
       <L.LoginSpeechBackground>
         <L.LoginTxt>어서와요!</L.LoginTxt>
+        <L.KakaoLogin src={KakaoLogin}></L.KakaoLogin>
+        <L.LoginSpeechArrow />
       </L.LoginSpeechBackground>
     </L.LoginBackBackground>
   );
--- frontend/src/components/login/LoginStyle.js
+++ frontend/src/components/login/LoginStyle.js
@@ -2,6 +2,7 @@
 import styled from "styled-components";
 import loginBg from "../../assets/image/loginBackground.gif";
 import loginSpeechBackground from "../../assets/image/loginSpeechBackground.png";
+import loginSpeechArrow from "../../assets/image/loginSpeechArrow.png";
 
 export const LoginBackBackground = styled.div`
   width: 100vw;
@@ -14,21 +15,38 @@ export const LoginBackBackground = styled.div`
 `;
 
 export const LoginSpeechBackground = styled.div`
-  width: 55vw;
-  height: 60vh;
-
+  width: 40vw;
+  height: 55vh;
   background-repeat: no-repeat;
-  object-fit: contain;
-  background-size: contain;
+  background-position: top right;
+  background-size: 100% 100%;
   background-image: url(${loginSpeechBackground});
+
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
 `;
 
+export const LoginSpeechArrow = styled.div`
+  position: absolute;
+
+  width: 15vw;
+  height: 10vh;
+  top: 30%;
+  left: 45%;
+  background-repeat: no-repeat;
+
+  background-size: 100% 100%;
+  background-image: url(${loginSpeechArrow});
+`;
+
 export const LoginTxt = styled.div`
-  width: 50%;
   height: 30%;
   font-size: 4vw;
+`;
+
+export const KakaoLogin = styled.img`
+  height: auto;
+  width: 60%;
 `;
