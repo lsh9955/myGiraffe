ccdb2d123df5eccbff2e05e928306d65bcb60ad3
dbsxowns2 <dbsxowns34@gmail.com>
2023-03-23 16:53:45 +0900

    feat: MyBook Entity 개발

---
+++ backend/user/src/main/java/com/ssafy/user/db/entity/MyBook.java
@@ -0,0 +1,26 @@
+package com.ssafy.user.db.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MyBook extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserInfo userinfo;
+
+    @Column(nullable = false)
+    private String bookName;
+
+    @Column(nullable = false)
+    private Long scenarioId;
+
+}
