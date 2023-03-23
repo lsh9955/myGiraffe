a30f23f06ba5a1182068bc2047f4c66a521f0ac5
dbsxowns2 <dbsxowns34@gmail.com>
2023-03-23 16:51:38 +0900

    feat: UserInfo Entity 개발

--- backend/user/src/main/java/com/ssafy/user/db/entity/UserInfo.java
+++ backend/user/src/main/java/com/ssafy/user/db/entity/UserInfo.java
@@ -1,28 +1,39 @@
 package com.ssafy.user.db.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-@Data
+@Data // Getter, Setter, RequiredArgsConstructor, toString
 @Entity
-public class UserInfo {
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // Parameter 없는 기본 생성자 생성 |
+// 속성 access : 기본 생성자, 정의한 생성자 를 제외한 임의의 객체생성 형태 불가
+public class UserInfo extends BaseTimeEntity {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-//  @Column(nullable = false, unique = true)
-  private Integer userId;
+  /**
+   * 📌Data Type
+   */
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long userId;
 
   @Column(nullable = false)
   private String userName;
 
-  @Column(nullable = true)
-  private String profileImageUrl;
+  @Column(nullable = false)
+  private String profileImage;
+
+  @Column(nullable = false)
+  @ColumnDefault("0")
+  private int coinAMount;
+
+//  @OneToMany(mappedBy = "userinfo", cascade = CascadeType.ALL)
+//  private List<MyBook> myBooks = new ArrayList<>();
 
 }
