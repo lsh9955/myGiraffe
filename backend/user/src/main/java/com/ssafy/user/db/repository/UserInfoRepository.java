a2e6e3894ff23b4883a4c0e8c92f5ee7222c7bc6
dbsxowns2 <dbsxowns34@gmail.com>
2023-03-24 14:57:53 +0900

    docs: UserInfoRepository 주석 제거

--- backend/user/src/main/java/com/ssafy/user/db/repository/UserInfoRepository.java
+++ backend/user/src/main/java/com/ssafy/user/db/repository/UserInfoRepository.java
@@ -9,6 +9,6 @@ import java.util.Optional;
 public interface UserInfoRepository extends JpaRepository<UserInfo, String> {
     Optional<UserInfo> findByUserId (String userId);
 
-    // 사용자 정보로 보유한 시나리오를 찾으려면 시나리오 DB에 접근 필요!
-    // * 유저가 보유한 시나리오 조회
+
+
 }
