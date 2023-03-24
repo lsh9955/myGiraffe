bd8673074545e6c158a9c17275e42ee00f11e854
hmschlng <soulb9@naver.com>
2023-03-24 10:07:22 +0900

    style: BaseResponseBody 설명 주석 추가
    BaseResponseBody에 대한 설명을 추가하였습니다.
    - api/dto/response/BaseResponseBody.java
    
    Issue: S08P22B201-435

--- backend/books/src/main/java/com/example/books/api/dto/response/BaseResponseBody.java
+++ backend/books/src/main/java/com/example/books/api/dto/response/BaseResponseBody.java
@@ -7,13 +7,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 
 /**
- * 응답 컨텐츠에 상태값을 함께 담아서 주기 위해 ResponseBody 에 들어갈 공통 body 양식을
- * 정의한 클래스입니다.
- *
- * 구성 요소는 다음과 같습니다.
- * - status:  상태 코드 넘버 (ex. 200)
- * - naming:  상태 코드 이름 (ex. OK)
- * - content: 컨텐츠        (ex. List<Object>)
+ * 응답 컨텐츠에 상태값을 함께 담아서 주기 위해 ResponseBody 에 들어갈 공통 body 양식을 정의한 클래스입니다.
+ * <p>
+ * 구성 요소는 다음과 같습니다.<p>
+ * - status:  상태 코드 넘버 (ex. 200)<p>
+ * - naming:  상태 코드 이름 (ex. OK) <p>
+ * - content: 컨텐츠        (ex. Object)
  *
  * @param <T>
  */
