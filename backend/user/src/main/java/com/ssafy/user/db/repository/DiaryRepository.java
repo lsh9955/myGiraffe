daee3e31fed30fcb00962b02fbe93a7bee0af552
dbsxowns2 <dbsxowns34@gmail.com>
2023-03-24 10:08:39 +0900

    feat: Diary Repository 개발

--- backend/user/src/main/java/com/ssafy/user/db/repository/DiaryRepository.java
+++ backend/user/src/main/java/com/ssafy/user/db/repository/DiaryRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 
 public interface DiaryRepository extends JpaRepository<Diary, Integer> {
 
-    List<Diary> findAllByUserId(String userId);
+    List<Diary> findAllByUserId(String userId); //
 
     Optional<Diary> findByDiaryId(Integer diaryId);
 
