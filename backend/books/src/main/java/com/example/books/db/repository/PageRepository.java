8364566e2df61fd4078aa6796950df1853760d45
hmschlng <soulb9@naver.com>
2023-03-29 01:15:57 +0900

    fix: GET 요청 시 리포지토리 함수 호출 오류 해결
    Page 엔티티에서 Foreign Key로 지정된 scenarioId 필드의 타입을 Scenario로 정정하고,
    이름을 scenario로 변경했습니다. 또한 그에 맞추어 칼럼명과 index 조회 칼럼명 등도 수정하였습니다.
    그리고 response DTO의 빌더 구현부도 그에 맞추어 수정했습니다.
    - db/entity/Page.java
    - db/repository/PageRepository.java
    - api/dto/response/PageGetResponse.java
    
    Issue: S08P22B201-622

--- backend/books/src/main/java/com/example/books/db/repository/PageRepository.java
+++ backend/books/src/main/java/com/example/books/db/repository/PageRepository.java
@@ -1,12 +1,13 @@
 package com.example.books.db.repository;
 
 import com.example.books.db.entity.Page;
+import com.example.books.db.entity.Scenario;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PageRepository extends JpaRepository<Page, Integer> {
-  Optional<Page> findByScenarioIdAndPageNo(Integer scenarioId, String pageNo);
+  Optional<Page> findByScenarioAndPageNo(Scenario scenario, String pageNo);
 
-  List<Page> findAllByScenarioId(Integer scenarioId);
+  List<Page> findAllByScenario(Scenario scenario);
 }
