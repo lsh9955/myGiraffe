baa6d0c05a348c51b4de93e85d99a4a29c99163c
hmschlng <soulb9@naver.com>
2023-03-29 01:01:43 +0900

    refactor: POST/PUT 요청 수신 방식 변경으로 인한 컨트롤러 로직 수정
    클라이언트 요청에 대해 @ModelAttribute 로 DTO를 받는 방식을 사용한 결과
    Validation 이전에 BindingException을 던지는 이슈가 발생했습니다.
    
    이를 기존의 Validation 디폴트메시지 형식의 Response로 주기 위해
    ControllerAdvice에서 ResponseEntityExceptionHandler.handleBindException 메소드를
    오버라이딩 하는 방식으로 해결하려 했으나, Spring 6 버전부터 해당 메소드가
    deprecated 되어 오버라이딩을 할 수 없었습니다.
      "as of 6.0 since org.springframework.web.method.
      annotation.ModelAttributeMethodProcessor now raises the
      MethodArgumentNotValidException subclass instead."
    
    때문에 컨트롤러에서 @ModelAttribute를 사용하는 대신 @RequestPart로 DTO와 MultipartFile을
    받고, PUT/POST를 받는 DTO에서 MultipartFile 필드를 제거하였습니다.
    - api/controller/PageController.java
    - api/controller/ScenarioController.java
    - api/dto/request/PagePostRequest.java
    - api/dto/request/PagePutRequest.java
    - api/dto/request/ScenarioPostRequest.java
    - api/dto/request/ScenarioPutRequest.java
    - api/service/impl/PageServiceImpl.java
    - api/service/impl/ScenarioServiceImpl.java
    
    Issue: S08P22B201-622

--- backend/books/src/main/java/com/example/books/api/dto/request/PagePostRequest.java
+++ backend/books/src/main/java/com/example/books/api/dto/request/PagePostRequest.java
@@ -1,13 +1,15 @@
 package com.example.books.api.dto.request;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import java.util.List;
-import lombok.Data;
-import org.springframework.web.multipart.MultipartFile;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Data
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PagePostRequest {
 
   @Positive(message = "필수 입력값입니다(양수).")
@@ -16,12 +18,9 @@ public class PagePostRequest {
   @Pattern(regexp = "^([1-9]*)-(\\d)*$", message = "'-'으로 구분되어야 합니다 (ex 3-0, 10-2).")
   private String pageNo;
 
-
   private String script;
-
-  private MultipartFile bgImgUrl;
 
   private List<String> nextPage;
 
-  private JsonNode objData;
+  private Object objData;
 }
--- backend/books/src/main/java/com/example/books/api/service/impl/PageServiceImpl.java
+++ backend/books/src/main/java/com/example/books/api/service/impl/PageServiceImpl.java
@@ -11,12 +11,13 @@ import com.example.books.db.repository.ScenarioRepository;
 import com.example.books.exception.BaseRuntimeException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -32,11 +33,11 @@ public class PageServiceImpl implements PageService {
   public PageGetResponse findPageByScenarioIdAndPageNo(Integer scenarioId, String pageNo) {
 
     // 페이지의 시나리오가 존재하는 지 확인
-    scenarioRepository.findById(scenarioId)
+    var scenario = scenarioRepository.findById(scenarioId)
         .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 시나리오 ID 입니다. (ID=" + scenarioId + ")"));
 
     // 페이지 조회와 동시에 존재 여부 검사
-    var page =  pageRepository.findByScenarioIdAndPageNo(scenarioId, pageNo)
+    var page =  pageRepository.findByScenarioAndPageNo(scenario, pageNo)
         .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 페이지입니다. (page=" + pageNo + ")"));
 
     // Page 를 PageGetResponse 로 빌드
@@ -49,11 +50,11 @@ public class PageServiceImpl implements PageService {
   public List<PageGetResponse> findAllPagesByScenarioId(Integer scenarioId) {
 
     // 페이지의 시나리오가 존재하는 지 확인
-    scenarioRepository.findById(scenarioId)
+    var scenario = scenarioRepository.findById(scenarioId)
         .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 시나리오 ID 입니다. (ID=" + scenarioId + ")"));
 
     // 페이지를 조회하면서 존재 여부 검사
-    var pages = pageRepository.findAllByScenarioId(scenarioId);
+    var pages = pageRepository.findAllByScenario(scenario);
 
     // 메인 서비스 컨텐츠인 페이지는 비어있으면 안돼므로 클라이언트에게 알림
     if(pages.isEmpty())
@@ -68,22 +69,26 @@ public class PageServiceImpl implements PageService {
   }
 
   @Override
-  public Integer savePage(PagePostRequest request) throws IOException {
+  public Integer savePage(PagePostRequest request, MultipartFile bgImg) throws IOException {
 
     // 시나리오가 존재하는 지 확인
     var scenario = scenarioRepository.findById(request.getScenarioId())
         .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 시나리오에 페이지를 추가할 수 없습니다. (ID=" + request.getScenarioId() + ")"));
 
+    // 페이지가 이미 존재하는 지 확인
+    pageRepository.findByScenarioAndPageNo(scenario, request.getPageNo())
+        .orElseThrow(() -> new IllegalArgumentException("이미 있는 페이지입니다."));
+
     // MultiPartFile -> String
-    var bgImgUrl = imageUrlProvider.getImageUrl(request.getBgImgUrl());
+    var bgImgUrl = imageUrlProvider.getImageUrl(bgImg);
     // List -> String
-    var nextPage = (request.getNextPage() != null) ? Arrays.toString(request.getNextPage().toArray()) : null;
+    var nextPage = objectMapper.writeValueAsString(request.getNextPage());
     // JsonNode -> String
-    var jsonString = objectMapper.writeValueAsString(request.getObjData());
+    var jsonString = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(request.getObjData());
 
     // PagePostRequest 를 Page 로 빌드
     var page = Page.builder()
-        .scenarioId(scenario)
+        .scenario(scenario)
         .pageNo(request.getPageNo())
         .script(request.getScript())
         .bgImgUrl(bgImgUrl)
@@ -96,31 +101,36 @@ public class PageServiceImpl implements PageService {
   }
 
   @Override
-  public Integer updatePage(PagePutRequest request) throws IOException {
+  public Integer updatePage(PagePutRequest request, MultipartFile bgImg) throws IOException {
 
     // 시나리오 존재 여부 확인
     var scenario = scenarioRepository.findById(request.getScenarioId())
         .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 시나리오 ID 입니다."));
 
     // 페이지 조회와 동시에 존재 여부 검사
-    var page =  pageRepository.findById(request.getPageId())
+    var page =  pageRepository.findByScenarioAndPageNo(scenario, request.getPageNo())
         .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 페이지입니다.)"));
 
     // MultiPartFile -> String
-    var bgImgUrl = imageUrlProvider.getImageUrl(request.getBgImgUrl());
+    var bgImgUrl = imageUrlProvider.getImageUrl(bgImg);
     // List -> String
-    var nextPage = (request.getNextPage() != null) ? Arrays.toString(request.getNextPage().toArray()) : null;
+    var nextPage = objectMapper.writeValueAsString(request.getNextPage());
     // JsonNode -> String
     var jsonString = objectMapper.writeValueAsString(request.getObjData());
 
     // 각 필드에 대해 null 이 아닌 경우 변경, null 이면 변경 X
     // toBuilder()와 리플렉션을 사용해 null 이 아닌 값만 빌드하는 방식도 생각해보기
     var updated = page.toBuilder()
-        .pageNo(request.getPageNo() != null ? request.getPageNo() : page.getPageNo())
-        .script(request.getScript() != null ? request.getScript() : page.getScript())
-        .bgImgUrl(bgImgUrl != null ? bgImgUrl : page.getBgImgUrl())
-        .nextPage(nextPage != null ? nextPage : page.getNextPage())
-        .objData(jsonString != null ? jsonString : page.getObjData())
+        .pageNo(Optional.ofNullable(request.getPageNo())
+            .orElse(page.getPageNo()))
+        .script(Optional.ofNullable(request.getScript())
+            .orElse(page.getScript()))
+        .bgImgUrl(Optional.ofNullable(bgImgUrl)
+            .orElse(page.getBgImgUrl()))
+        .nextPage(Optional.ofNullable(nextPage)
+            .orElse(page.getNextPage()))
+        .objData(Optional.ofNullable(jsonString)
+            .orElse(page.getObjData()))
         .build();
 
     // DB 에 저장하고 ID 값을 반환
