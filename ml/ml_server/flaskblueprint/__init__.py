188cbc12aeda7d7950ee70b2455a13eb3ba53ec5
DataMarksman <zkstizn@gmail.com>
2023-03-29 17:41:27 +0900

    bugfix: module error fix

--- ml/ml_server/flaskblueprint/__init__.py
+++ ml/ml_server/flaskblueprint/__init__.py
@@ -7,7 +7,6 @@ from .aivoice_blueprint import aivoice_blueprint
 from .classifier_blueprint import classifier_blueprint
 from .numbers_blueprint import numbers_blueprint
 from .visionapi_blueprint import visionapi_blueprint
-from .webdetection_blueprint import webdetection_blueprint
 
 
 # Flask 애플리케이션에서 사용할 Blueprint 객체를 생성합니다.
@@ -16,5 +15,4 @@ app_blueprint = [
     classifier_blueprint,
     numbers_blueprint,
     visionapi_blueprint,
-    webdetection_blueprint
 ]
