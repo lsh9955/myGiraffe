a66e1d782ea2ff7c6d9359ed436147bf4235217b
dbsxowns2 <dbsxowns34@gmail.com>
2023-03-29 09:12:14 +0900

    feat: Sketch Service 개발

---
+++ backend/user/src/main/java/com/ssafy/user/api/service/SketchService.java
@@ -0,0 +1,40 @@
+package com.ssafy.user.api.service;
+
+import com.ssafy.user.api.dto.request.SketchPostRequest;
+import com.ssafy.user.db.entity.Sketch;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface SketchService {
+
+    /**
+     * 스케치북 전체 조회
+     * @param userId
+     * @return 해당 ID의 유저 스케치북들
+     */
+    List<Sketch> findSketchesByUserId(String userId);
+
+
+    /**
+     * 스케치북 하나 조회
+     * @param sketchId
+     * @return 해당 스케치
+     */
+    Sketch findSketchById(Integer sketchId);
+
+
+    /**
+     * 스케치북 저장
+     * @param SketchPostRequest request
+     * @return 해당 스케치북 ID
+     */
+    Integer saveSketch(SketchPostRequest request) throws IOException;
+
+
+    /**
+     * 스케치북 삭제
+     * @param sketchId;
+     */
+    void deleteSketch(Integer sketchId);
+}
