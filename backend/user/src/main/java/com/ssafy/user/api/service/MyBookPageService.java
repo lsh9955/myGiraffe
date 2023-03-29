e3be62cdc688e7d78a0a1144d4d8aa6fda780724
dbsxowns2 <dbsxowns34@gmail.com>
2023-03-29 09:10:23 +0900

    feat: MyBookPage Service 개발

---
+++ backend/user/src/main/java/com/ssafy/user/api/service/MyBookPageService.java
@@ -0,0 +1,41 @@
+package com.ssafy.user.api.service;
+
+import com.ssafy.user.api.dto.request.MyBookPagePostRequest;
+import com.ssafy.user.db.entity.MyBookPage;
+
+import java.util.List;
+
+public interface MyBookPageService {
+
+    /**
+     * 내 동화책 ID 와 페이지 No를 기준으로 페이지를 조회합니다.
+     * @param bookId
+     * @param pageNo
+     * @return 해당 동화책과 페이지 번호를 가진 페이지
+     */
+    MyBookPage findPageByBookIdAndPageNo(Integer bookId, String pageNo);
+
+
+    /**
+     * 내 동화책 ID 를 가진 모든 페이지를 조회합니다.
+     * @param bookId
+     * @return 해당 내 동화책 페이지들
+     */
+    List<MyBookPage> findAllPagesByBookId(Integer bookId);
+
+
+    /**
+     * 사용자가 수행한 상호작용 정보 포함한 동화책 페이지 정보를 저장합니다.
+     * @param MyBookPagePostRequest request
+     * @return 해당 내 동화책 pageId
+     */
+    Integer savePage(MyBookPagePostRequest request);
+
+
+    /**
+     * 해당 내 동화책 페이지 ID를 가진 페이지를 삭제합니다.
+     * @param pageId
+     */
+    void deletePage(Integer pageId);
+
+}
