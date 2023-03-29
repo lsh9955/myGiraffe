7b8c640757f75098f189f9070d3428217aa5443d
dbsxowns2 <dbsxowns34@gmail.com>
2023-03-29 09:23:11 +0900

    feat: Sketch Controller 개발

---
+++ backend/user/src/main/java/com/ssafy/user/api/controller/SketchController.java
@@ -0,0 +1,83 @@
+package com.ssafy.user.api.controller;
+
+import com.ssafy.user.api.dto.request.SketchPostRequest;
+import com.ssafy.user.api.dto.response.BaseResponseBody;
+import com.ssafy.user.api.service.SketchService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.executable.ValidateOnExecution;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.net.URI;
+
+@Slf4j
+@Validated
+@ValidateOnExecution
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/members")
+public class SketchController {
+
+    private final SketchService sketchService;
+
+    @GetMapping("/sketch/{userId}")
+    public ResponseEntity<? extends BaseResponseBody> getSketches(
+            @PathVariable("userId") String userId) {
+
+        var sketches = sketchService.findSketchesByUserId(userId);
+
+        return ResponseEntity
+                .ok()
+                .body(new BaseResponseBody(200, "OK", sketches));
+    }
+
+    @GetMapping("/sketch/{sketchId}")
+    public ResponseEntity<? extends BaseResponseBody> getSketch(
+            @PathVariable("sketchId") Integer sketchId) {
+
+        var sketch = sketchService.findSketchById(sketchId);
+
+        return ResponseEntity
+                .ok()
+                .body(new BaseResponseBody(200, "OK", sketch));
+    }
+
+
+    @PostMapping
+    public ResponseEntity<? extends BaseResponseBody> createSketch(
+            @ModelAttribute
+            @Valid
+            SketchPostRequest sketch,
+            HttpServletRequest request) throws IOException {
+
+        var id = sketchService.saveSketch(sketch);
+
+        var location = URI.create(request.getRequestURI() + "/" + id);
+        var successMessage = "스케치북 생성 성공: (ID=" + id + ")";
+
+        return ResponseEntity
+                .created(location)
+                .body(new BaseResponseBody<>(201, "Created", successMessage));
+    }
+
+    @DeleteMapping("/sketch/{sketchId}")
+    public ResponseEntity<? extends BaseResponseBody> deleteSketch(
+            @PathVariable
+            @Positive(message = "필수 입력값 입니다(양수).")
+            Integer sketchId) {
+
+        sketchService.deleteSketch(sketchId);
+
+        var successMessage = "스케치북이 성공적으로 삭제되었습니다: (ID=" + sketchId + ")";
+
+        return ResponseEntity
+                .ok()
+                .body(new BaseResponseBody<>(200, "OK", successMessage));
+    }
+}
