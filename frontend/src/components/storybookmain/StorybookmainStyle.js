13975d9be583958bb82a8966c52df1716e14b774
이석훈 <cqe9933@naver.com>
2023-03-29 16:54:14 +0900

    ✨ feat: 동화책 선택 페이지 구현

--- frontend/src/components/storybookmain/StorybookmainStyle.js
+++ frontend/src/components/storybookmain/StorybookmainStyle.js
@@ -1,3 +1,135 @@
-/**동화책 선택 컴포넌트 style*/
-// import styled from "styled-components";
-export {};
+/**책장 메인화면 컴포넌트 style */
+import styled from "styled-components";
+
+export const MydrawerContainer = styled.div`
+  width: 100vw;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const MydrawerListContainer = styled.div`
+  width: auto;
+  height: 30%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-around;
+  border: 2px solid #000000;
+`;
+
+export const MydraweritemContainer = styled.div`
+  position: relative;
+  text-align: center;
+`;
+
+export const TextOverlay = styled.div`
+  position: absolute;
+
+  top: 30%;
+
+  width: 90%;
+  max-height: 40%;
+  /* transform: translate(-35%, -40%); */
+  color: white;
+  text-shadow: 1px 1px 2px black;
+  & > div {
+    width: 100%;
+    max-height: 40%;
+    font-size: 200%;
+    margin-bottom: 5%;
+  }
+  & > p {
+    width: 100%;
+    font-size: 1vw;
+  }
+`;
+
+export const Mydraweritemimage = styled.img`
+  width: 90%;
+  /* width: 200px;
+  height: 292px; */
+`;
+
+export const MySketchbookimage = styled.img`
+  width: 90%;
+  height: 50vh;
+  border-radius: 40px;
+  /* width: 400px;
+  height: 274px; */
+`;
+
+export const Container = styled.div`
+  width: 70%;
+  height: 80%;
+  margin: 0 auto;
+  margin-top: 10vh;
+`;
+
+export const TitleContainer = styled.p`
+  font-size: 4rem;
+  text-align: center;
+  margin-top: 10px;
+  margin-bottom: 20px;
+`;
+
+export const BeforeBtn = styled.div`
+  font-size: 0;
+  line-height: 0;
+  position: absolute;
+  top: 50%;
+  font-size: 30px;
+  display: block;
+  width: 20px;
+  height: 20px;
+  padding: 0;
+  -webkit-transform: translate(0, -50%);
+  transform: translate(0, -50%);
+  cursor: pointer;
+  color: transparent;
+  border: none;
+  outline: none;
+  background: transparent;
+`;
+export const AfterBtn = styled.div`
+  font-size: 0;
+  line-height: 0;
+  font-size: 30px;
+  position: absolute;
+  top: 50%;
+  display: block;
+  width: 20px;
+  height: 20px;
+  padding: 0;
+  -webkit-transform: translate(0, -50%);
+  transform: translate(0, -50%);
+  cursor: pointer;
+  color: transparent;
+  border: none;
+  outline: none;
+  background: transparent;
+`;
+
+export const ImgTitle = styled.div`
+  width: 90%;
+  font-size: 200%;
+`;
+export const ImgP = styled.p`
+  width: 90%;
+  font-size: 150%;
+`;
+
+export const LockImg = styled.img`
+  position: absolute;
+  z-index: 3;
+  top: 35%;
+  left: 30%;
+`;
+export const MySketchbookLockimage = styled.img`
+  width: 90%;
+  height: 50vh;
+  border-radius: 40px;
+  filter: brightness(50%);
+`;
