d06041f9e65839e8357a4cd7e39f9e1392fae895
dbsxowns2 <dbsxowns34@gmail.com>
2023-03-30 09:34:32 +0900

    feat: MyBookPage Repository 메서드 파라미터 수정

--- backend/user/src/main/java/com/ssafy/user/db/repository/MyBookPageRepository.java
+++ backend/user/src/main/java/com/ssafy/user/db/repository/MyBookPageRepository.java
@@ -1,5 +1,6 @@
 package com.ssafy.user.db.repository;
 
+import com.ssafy.user.db.entity.MyBook;
 import com.ssafy.user.db.entity.MyBookPage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MyBookPageRepository extends JpaRepository<MyBookPage, Integer> {
-    Optional<MyBookPage> findByBookIdAndPageNo(Integer bookId, String pageNo);
+    Optional<MyBookPage> findByMyBookAndPageNo(MyBook myBook, String pageNo);
 
-    List<MyBookPage> findAllByBookId(Integer bookId);
+    List<MyBookPage> findAllByMyBook(MyBook myBook);
 }
