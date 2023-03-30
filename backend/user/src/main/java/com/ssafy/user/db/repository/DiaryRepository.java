1007c78256aa4f9ee8715a3c22c648e09bf6b0c2
dbsxowns2 <dbsxowns34@gmail.com>
2023-03-30 09:31:18 +0900

    feat: Diary Repository 메서드 파라미터 수정 / 기본 메서드 삭제

--- backend/user/src/main/java/com/ssafy/user/db/repository/DiaryRepository.java
+++ backend/user/src/main/java/com/ssafy/user/db/repository/DiaryRepository.java
@@ -1,6 +1,7 @@
 package com.ssafy.user.db.repository;
 
 import com.ssafy.user.db.entity.Diary;
+import com.ssafy.user.db.entity.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,8 +9,6 @@ import java.util.Optional;
 
 public interface DiaryRepository extends JpaRepository<Diary, Integer> {
 
-    List<Diary> findAllByUserId(String userId); //
-
-    Optional<Diary> findByDiaryId(Integer diaryId);
+    List<Diary> findAllByUserInfo(UserInfo userInfo);
 
 }
