d832e65c711c5082dc8128965b3cee7e93fa496f
dbsxowns2 <dbsxowns34@gmail.com>
2023-03-30 09:36:59 +0900

    feat: MyBook Repository 메서드 파라미터 수정 / 기본 메서드 삭제

--- backend/user/src/main/java/com/ssafy/user/db/repository/MyBookRepository.java
+++ backend/user/src/main/java/com/ssafy/user/db/repository/MyBookRepository.java
@@ -1,6 +1,7 @@
 package com.ssafy.user.db.repository;
 
 import com.ssafy.user.db.entity.MyBook;
+import com.ssafy.user.db.entity.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,8 +9,6 @@ import java.util.Optional;
 
 public interface MyBookRepository extends JpaRepository<MyBook, Integer> {
 
-    List<MyBook> findALlByUserId(String userId);
-
-    Optional<MyBook> findByBookId(Integer bookId);
+    List<MyBook> findAllByUserInfo(UserInfo userId);
 
 }
