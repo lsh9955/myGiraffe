a791fabcb95df72f8989896a6de173a593b7d654
dbsxowns2 <dbsxowns34@gmail.com>
2023-03-30 09:52:57 +0900

    feat: Sketch Post 요청 어노테이션 수정

--- backend/user/src/main/java/com/ssafy/user/api/dto/request/SketchPostRequest.java
+++ backend/user/src/main/java/com/ssafy/user/api/dto/request/SketchPostRequest.java
@@ -1,12 +1,13 @@
 package com.ssafy.user.api.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class SketchPostRequest {
 
-    @NotEmpty(message = "필수 입력 항목입니다.")
+    @NotBlank(message = "필수 입력 항목입니다.")
     private String userId;
 
     private String sketchName;
