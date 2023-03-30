376ddb21e0df4fe1a34cf2f5cffc59032099b478
dbsxowns2 <dbsxowns34@gmail.com>
2023-03-30 09:54:00 +0900

    feat: Sketch Repository 메서드 파라미터 수정 / 기본 메서드 삭제

--- backend/user/src/main/java/com/ssafy/user/db/repository/SketchRepository.java
+++ backend/user/src/main/java/com/ssafy/user/db/repository/SketchRepository.java
@@ -1,6 +1,7 @@
 package com.ssafy.user.db.repository;
 
 import com.ssafy.user.db.entity.Sketch;
+import com.ssafy.user.db.entity.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,9 +9,6 @@ import java.util.Optional;
 
 public interface SketchRepository extends JpaRepository<Sketch, Integer> {
 
-    List<Sketch> findAllByUserId(String userId);
-
-    Optional<Sketch> findBySketchId(Integer sketchId);
-
+    List<Sketch> findAllByUserInfo(UserInfo userInfo);
 
 }
