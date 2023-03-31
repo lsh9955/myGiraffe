e2a67c241b96ddd094ac3cd0993a819fe3012453
dbsxowns2 <dbsxowns34@gmail.com>
2023-03-31 15:26:37 +0900

    feat: 동화책 페이지 Post 요청 Dto 필드 및 어노테이션 수정

--- backend/user/src/main/java/com/ssafy/user/api/dto/request/MyBookPagePostRequest.java
+++ backend/user/src/main/java/com/ssafy/user/api/dto/request/MyBookPagePostRequest.java
@@ -1,31 +1,31 @@
 package com.ssafy.user.api.dto.request;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.web.multipart.MultipartFile;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
-import java.util.Map;
 
-@Data
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class MyBookPagePostRequest {
 
     @Positive(message = "필수 입력 항목입니다(양수).")
     private Integer bookId;
 
-    @Pattern(regexp = "^([1-9]*)-(\\d)*$", message = "'-'으로 구분되어야 합니다 (ex 3-0, 10-2).")
+    @Pattern(regexp = "^(\\d)*-(\\d)*$", message = "'-'으로 구분되어야 합니다 (ex 3-0, 10-2).")
     private String pageNo;
 
     private String script;
 
-    private MultipartFile bgImgUrl;
-
     private List<String> nextPage;
 
-    private JsonNode objData;
+    private Object objData;
 
-    private Map<MultipartFile, JsonNode> objUserData;
+    private Object objUserData;
 }
