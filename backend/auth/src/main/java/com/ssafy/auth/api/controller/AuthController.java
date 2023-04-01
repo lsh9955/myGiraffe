cb88bdf34767252a3ae0be403025f6b84259b9bd
MJun111 <kmj0568@gmail.com>
2023-04-01 19:19:01 +0900

    feature: api gateway 토큰 검증 추가 api gateway에서 토큰을 검증하고 요청 헤더에 토큰 내 정보를 추가하였습니다. spring server에 대한 모든 요청을 gateway에서 관리하고 라우팅하도록 수정했습니다.

--- backend/auth/src/main/java/com/ssafy/auth/api/controller/AuthController.java
+++ backend/auth/src/main/java/com/ssafy/auth/api/controller/AuthController.java
@@ -1,13 +1,16 @@
 package com.ssafy.auth.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/api/auth")
 public class AuthController {
 
-	@GetMapping
-	boolean checkAccessToken() {
+	@GetMapping("/{token}")
+	boolean checkAccessToken(@PathVariable String token) {
+		System.out.println(token);
 		return true;
 	}
 
--- backend/gateway/src/main/java/com/ssafy/gateway/config/AuthorizationHeaderFilter.java
+++ backend/gateway/src/main/java/com/ssafy/gateway/config/AuthorizationHeaderFilter.java
@@ -1,7 +1,10 @@
 package com.ssafy.gateway.config;
 
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
 
+import com.ssafy.gateway.util.JwtUtil;
+import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
@@ -13,16 +16,14 @@ import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 
-import com.ssafy.gateway.client.TokenCheckClient;
-
-import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Component
 public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
 
-    private TokenCheckClient tokenCheckClient;
+    @Autowired
+    private JwtUtil jwtUtil;
 
     public AuthorizationHeaderFilter() {
         super(Config.class);
@@ -37,11 +38,9 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
         return (exchange, chain) -> {
             String token = exchange.getRequest().getHeaders().get("Authorization").get(0).substring(7);   // 헤더의 토큰 파싱 (Bearer 제거)
 
-            tokenCheckClient.checkAccessToken();
+            String userId = jwtUtil.getUid(token);
 
-            // String userId = jwtUtil.getUid(token);
-
-            // addAuthorizationHeaders(exchange.getRequest(), userId);
+            addAuthorizationHeaders(exchange.getRequest(), userId);
             return chain.filter(exchange);
         };
     }
@@ -53,45 +52,50 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
                 .build();
     }
 
-    // // 토큰 검증 요청을 실행하는 도중 예외가 발생했을 때 예외처리하는 핸들러
-    // @Bean
-    // public ErrorWebExceptionHandler tokenValidation() {
-    //     return new JwtTokenExceptionHandler();
-    // }
-    //
-    // // 실제 토큰이 null, 만료 등 예외 상황에 따른 예외처리
-    // public class JwtTokenExceptionHandler implements ErrorWebExceptionHandler {
-    //     private String getErrorCode(int errorCode) {
-    //         return "{\"errorCode\":" + errorCode + "}";
-    //     }
-    //
-    //     @Override
-    //     public Mono<Void> handle(
-    //             ServerWebExchange exchange, Throwable ex) {
-    //
-    //         int errorCode = 500;
-    //         if (ex.getClass() == NullPointerException.class) {
-    //             errorCode = 100;
-    //         } else if (ex.getClass() == ExpiredJwtException.class) {
-    //             errorCode = 200;
-    //         }
-    //
-    //         byte[] bytes = getErrorCode(errorCode).getBytes(StandardCharsets.UTF_8);
-    //         DataBuffer buffer = exchange.getResponse().bufferFactory().wrap(bytes);
-    //
-    //          // 토큰 값 null 일 시에 401 or 만료 시에 403 에러 발생
-    //          if (errorCode == 100) {
-    //              exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-    //              return exchange.getResponse().writeWith(Flux.just(buffer));
-    //          }
-    //          else if (errorCode == 200) {
-    //              exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
-    //              return exchange.getResponse().writeWith(Flux.just(buffer));
-    //          }
-    //          else {
-    //              exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-    //              return exchange.getResponse().writeWith(Flux.just(buffer));
-    //          }
-    //     }
-    // }
+    // 토큰 검증 요청을 실행하는 도중 예외가 발생했을 때 예외처리하는 핸들러
+     @Bean
+     public ErrorWebExceptionHandler tokenValidation() {
+         return new JwtTokenExceptionHandler();
+     }
+
+     // 실제 토큰이 null, 만료 등 예외 상황에 따른 예외처리
+     public class JwtTokenExceptionHandler implements ErrorWebExceptionHandler {
+         private String getErrorCode(int errorCode) {
+             if (errorCode == 100)
+                return "{\n 토큰이 없습니다. \n\"errorCode\":" + errorCode + "\n}";
+             else if (errorCode == 200)
+                 return "{\n 토큰이 만료되었습니다. \n\"errorCode\":" + errorCode + "\n}";
+             else
+                 return "{\n 토큰에 관한 예외사항이 발생하였습니다. \n\"errorCode\":" + errorCode + "\n}";
+         }
+
+         @Override
+         public Mono<Void> handle(
+                 ServerWebExchange exchange, Throwable ex) {
+
+             int errorCode = 500;
+             if (ex.getClass() == NullPointerException.class) {
+                 errorCode = 100;
+             } else if (ex.getClass() == ExpiredJwtException.class) {
+                 errorCode = 200;
+             }
+
+             byte[] bytes = getErrorCode(errorCode).getBytes(StandardCharsets.UTF_8);
+             DataBuffer buffer = exchange.getResponse().bufferFactory().wrap(bytes);
+
+              // 토큰 값 null 일 시에 401 or 만료 시에 403 에러 발생
+              if (errorCode == 100) {
+                  exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                  return exchange.getResponse().writeWith(Flux.just(buffer));
+              }
+              else if (errorCode == 200) {
+                  exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
+                  return exchange.getResponse().writeWith(Flux.just(buffer));
+              }
+              else {
+                  exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                  return exchange.getResponse().writeWith(Flux.just(buffer));
+              }
+         }
+     }
 }
