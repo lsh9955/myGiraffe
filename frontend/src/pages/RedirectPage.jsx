4b301133755116f3a3d042c5044d008808dce6c8
이석훈 <cqe9933@naver.com>
2023-04-02 16:18:10 +0900

    🚑 fix:로그인 로직 재구현

--- frontend/src/pages/RedirectPage.jsx
+++ frontend/src/pages/RedirectPage.jsx
@@ -7,10 +7,31 @@ const RedirectPage = () => {
   const history = useHistory();
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(
-      login({ accessToken: location.search.split("?Authorization=")[1] })
-    );
-    history.push("/");
+    const getUserInfo = async () => {
+      await axios
+        .get("http://j8b201.p.ssafy.io/api/members", {
+          headers: {
+            Authorization: location.search.split("?Authorization=")[1],
+          },
+        })
+        .then((response) => {
+          dispatch(
+            login({
+              accessToken: location.search.split("?Authorization=")[1],
+              userId: response.data.content.userId,
+              userName: response.data.content.userName,
+              profileImg: response.data.content.profileImg,
+              coinAmount: response.data.content.coinAmount,
+              accessToken: response.data.content.accessToken,
+            })
+          );
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      getUserInfo();
+      history.push("/");
+    };
   }, []);
 
   return <div>RedirectPage</div>;
--- frontend/src/store/AuthSlice.js
+++ frontend/src/store/AuthSlice.js
@@ -1,68 +1,33 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 // 초기값 설정
 // authenticated : 로그인 여부
 // user_name, user_id : 유저 정보
 const initialStateValue = {
-  userSeq: null,
-  authenticated: false,
-  userAccountStatus: null,
-  userCookCategory: null,
-  userCreateDate: null,
-  userEmail: null,
   userId: null,
-  userImg: null,
-  userIntroduce: null,
-  userLastLoginDate: null,
   userName: null,
-  userNickname: null,
-  userRoleType: null,
-  userSnsType: null,
-  userTemp: null,
+  profileImg: null,
+  coinAmount: null,
   accessToken: null,
 };
 // slice 이름을 user로 함
 // login, logout, register로 구분하여 각각 해당하는 값을 action에서 전달
 export const AuthSlice = createSlice({
-  name: 'user',
+  name: "user",
   initialState: initialStateValue,
   reducers: {
     login: (state, action) => {
-      const changeState = state;
-      changeState.userSeq = action.payload.userSeq;
-      changeState.authenticated = action.payload.authenticated;
-      changeState.userAccountStatus = action.payload.userAccountStatus;
-      changeState.userCookCategory = action.payload.userCookCategory;
-      changeState.userCreateDate = action.payload.userCreateDate;
-      changeState.userEmail = action.payload.userEmail;
-      changeState.userId = action.payload.userId;
-      changeState.userImg = action.payload.userImg;
-      changeState.userIntroduce = action.payload.userIntroduce;
-      changeState.userLastLoginDate = action.payload.userLastLoginDate;
-      changeState.userName = action.payload.userName;
-      changeState.userNickname = action.payload.userNickname;
-      changeState.userRoleType = action.payload.userRoleType;
-      changeState.userSnsType = action.payload.userSnsType;
-      changeState.userTemp = action.payload.userTemp;
-      changeState.accessToken = action.payload.accessToken;
+      state.userId = action.payload.userId;
+      state.userName = action.payload.userName;
+      state.profileImg = action.payload.profileImg;
+      state.coinAmount = action.payload.coinAmount;
+      state.accessToken = action.payload.accessToken;
     },
     logout: (state, action) => {
-      const changeState = state;
-      changeState.userSeq = null;
-      changeState.authenticated = false;
-      changeState.userAccountStatus = null;
-      changeState.userCookCategory = null;
-      changeState.userCreateDate = null;
-      changeState.userEmail = null;
-      changeState.userId = null;
-      changeState.userImg = null;
-      changeState.userIntroduce = null;
-      changeState.userLastLoginDate = null;
-      changeState.userName = null;
-      changeState.userNickname = null;
-      changeState.userRoleType = null;
-      changeState.userSnsType = null;
-      changeState.userTemp = null;
-      changeState.accessToken = null;
+      userId = null;
+      userName = null;
+      profileImg = null;
+      coinAmount = null;
+      accessToken = null;
     },
   },
 });
