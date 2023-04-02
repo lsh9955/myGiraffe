e74534dc6e55279c6c4a5ea44b89d341c2baa71a
MJun111 <kmj0568@gmail.com>
2023-04-03 00:12:26 +0900

    fix: cors 설정 추가 gateway filter에 cors 방지 설정을 추가했습니다.

--- backend/gateway/src/main/java/com/ssafy/gateway/config/PassCorsRoutePredicateHandlerMapping.java
+++ backend/gateway/src/main/java/com/ssafy/gateway/config/PassCorsRoutePredicateHandlerMapping.java
@@ -6,8 +6,6 @@ import org.springframework.cloud.gateway.config.GlobalCorsProperties;
 import org.springframework.cloud.gateway.handler.FilteringWebHandler;
 import org.springframework.cloud.gateway.handler.RoutePredicateHandlerMapping;
 import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -28,7 +26,6 @@ public class PassCorsRoutePredicateHandlerMapping extends RoutePredicateHandlerM
             logger.info(exchange.getLogPrefix() + "Mapped to " + handler);
 
             // CORS 체크 로직 제거
-
             return handler;
         });
     }
