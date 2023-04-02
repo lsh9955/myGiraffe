38a4f98971fbcc8b5b64894f5eb0de59f0b72f28
MJun111 <kmj0568@gmail.com>
2023-04-02 21:05:28 +0900

    fix: cors 설정 추가 cors 발생을 방지하기 위해 config를 추가하고 재정의하였습니다.

--- backend/gateway/src/main/java/com/ssafy/gateway/config/CustomCorsConfiguration.java
+++ backend/gateway/src/main/java/com/ssafy/gateway/config/CustomCorsConfiguration.java
@@ -6,10 +6,12 @@ import org.springframework.cloud.gateway.handler.RoutePredicateHandlerMapping;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
 
 @Configuration
 public class CustomCorsConfiguration {
+    @Primary
     @Bean
     public RoutePredicateHandlerMapping passCorsRoutePredicateHandlerMapping(
             FilteringWebHandler webHandler, RouteLocator routeLocator,
