6883a26aaddc31f5bc8f75f27d5432f635fc90b1
MJun111 <kmj0568@gmail.com>
2023-04-02 20:47:22 +0900

    cors 처리 cors가 발생하지 않도록 일부 config를 재정의 하였습니다.

---
+++ backend/gateway/src/main/java/com/ssafy/gateway/config/CustomCorsConfiguration.java
@@ -0,0 +1,20 @@
+package com.ssafy.gateway.config;
+
+import org.springframework.cloud.gateway.config.GlobalCorsProperties;
+import org.springframework.cloud.gateway.handler.FilteringWebHandler;
+import org.springframework.cloud.gateway.handler.RoutePredicateHandlerMapping;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class CustomCorsConfiguration {
+    @Bean
+    public RoutePredicateHandlerMapping passCorsRoutePredicateHandlerMapping(
+            FilteringWebHandler webHandler, RouteLocator routeLocator,
+            GlobalCorsProperties globalCorsProperties, Environment environment) {
+        return new PassCorsRoutePredicateHandlerMapping(webHandler, routeLocator,
+                globalCorsProperties, environment);
+    }
+}
