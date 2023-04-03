cd181b85cafaf0ae384b1c78416d169d55f88505
김명준 <kmj0568@gmail.com>
2023-04-03 11:06:43 +0900

    fix: 로그인 과정 검증 제외

--- backend/user/src/main/java/com/ssafy/user/config/CustomAuthorizationFilter.java
+++ backend/user/src/main/java/com/ssafy/user/config/CustomAuthorizationFilter.java
@@ -30,27 +30,28 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 		if (request.getServletPath().equals("/api/members/user") || request.getServletPath().equals("/api/members/image")) {    // 로그인은 그냥 건너 뛴다
 			filterChain.doFilter(request, response);
+		} else {
+
+			String token = request.getHeader("Authorization");   // 헤더의 토큰 파싱 (Bearer 제거)
+			try {
+				String userId = jwtUtil.getUid(token);
+				addAuthorizationHeaders(request, userId);
+
+				Authentication auth = getAuthentication(userId);
+				SecurityContextHolder.getContext().setAuthentication(auth);
+			} catch (Exception e) {
+				response.setStatus(HttpStatus.UNAUTHORIZED.value());
+				response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+				Map<String, Object> body = new LinkedHashMap<>();
+				body.put("code", HttpStatus.UNAUTHORIZED.value());
+				body.put("error", e.getMessage());
+
+				new ObjectMapper().writeValue(response.getOutputStream(), body);
+			}
+
+			filterChain.doFilter(request, response);
 		}
-
-		String token = request.getHeader("Authorization");   // 헤더의 토큰 파싱 (Bearer 제거)
-		try {
-			String userId = jwtUtil.getUid(token);
-			addAuthorizationHeaders(request, userId);
-
-			Authentication auth = getAuthentication(userId);;
-			SecurityContextHolder.getContext().setAuthentication(auth);
-		} catch (Exception e) {
-			response.setStatus(HttpStatus.UNAUTHORIZED.value());
-			response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-
-			Map<String, Object> body = new LinkedHashMap<>();
-			body.put("code", HttpStatus.UNAUTHORIZED.value());
-			body.put("error", e.getMessage());
-
-			new ObjectMapper().writeValue(response.getOutputStream(), body);
-		}
-
-		filterChain.doFilter(request, response);
 	}
 
 	// 성공적으로 검증이 되었기 때문에 인증된 헤더로 요청을 변경해준다. 서비스는 해당 헤더에서 아이디를 가져와 사용한다.
