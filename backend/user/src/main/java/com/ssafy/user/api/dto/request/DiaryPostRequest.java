8f69674839deb93dd9006efcf2163c95d064ef9a
hmschlng <soulb9@naver.com>
2023-04-04 13:37:27 +0900

    fix: Object에 대한 validation 해제
    Object인 필드에 String 값 validation이 붙어 있어 해제했습니다.
    
    Issue:

--- backend/user/src/main/java/com/ssafy/user/api/dto/request/DiaryPostRequest.java
+++ backend/user/src/main/java/com/ssafy/user/api/dto/request/DiaryPostRequest.java
@@ -6,6 +6,5 @@ import lombok.Data;
 @Data
 public class DiaryPostRequest {
 
-  @NotBlank(message = "필수 항목 입니다.")
   private Object diaryTraceData;
 }
--- backend/user/src/main/java/com/ssafy/user/api/dto/request/SketchPostRequest.java
+++ backend/user/src/main/java/com/ssafy/user/api/dto/request/SketchPostRequest.java
@@ -6,7 +6,5 @@ import lombok.Data;
 @Data
 public class SketchPostRequest {
 
-  @NotNull(message = "필수 입력 항목입니다.")
   private Object sketchTraceData;
-
 }
