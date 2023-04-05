b5191963e4fcb43a21919388e545f437ddedcf1b
hmschlng <soulb9@naver.com>
2023-04-05 14:20:37 +0900

    fix: 머지 오류 해결

--- backend/user/src/main/java/com/ssafy/user/db/entity/UserInfo.java
+++ backend/user/src/main/java/com/ssafy/user/db/entity/UserInfo.java
@@ -17,19 +17,41 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 
 
-@Data
+//@Data
+// Getter, Setter, RequiredArgsConstructor, toString
+@Getter
+@Builder(toBuilder = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // Parameter 없는 기본 생성자 생성 |
+@AllArgsConstructor
+@Table(name = "user_info")
 @Entity
-public class UserInfo {
+// 속성 access : 기본 생성자, 정의한 생성자 를 제외한 임의의 객체생성 형태 불가
+public class UserInfo extends BaseTimeEntity {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-//  @Column(nullable = false, unique = true)
-  private Integer userId;
+  @Column(name = "user_id")
+  private String userId;
 
-  @Column(nullable = false)
+  @Column(name = "user_name", nullable = false)
   private String userName;
 
-  @Column(nullable = true)
-  private String profileImageUrl;
+  @Column(name = "profile_img", nullable = false)
+  private String profileImg;
+
+  @Column(name = "coin_amount", nullable = false)
+  @ColumnDefault("0")
+  private int coinAMount;
+
+  @OneToMany(mappedBy = "userInfo", cascade = CascadeType.ALL)
+  @JsonManagedReference
+  private List<MyBook> myBooks = new ArrayList<>();
+
+  @OneToMany(mappedBy = "userInfo", cascade = CascadeType.ALL)
+  @JsonManagedReference
+  private List<Diary> diaries = new ArrayList<>();
+
+  @OneToMany(mappedBy = "userInfo", cascade = CascadeType.ALL)
+  @JsonManagedReference
+  private List<Sketch> sketches = new ArrayList<>();
 
 }
