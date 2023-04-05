d70c7fee872b6e02acf8a7fe79470d6dc7df241e
pinetree-1017 <pinetree-1017@naver.com>
2023-04-05 09:58:00 +0900

    ⚡️ feat : 내 서랍장 그림일기, 스케치북 모달 구현

--- frontend/src/components/mydrawer/mysketchbook/MySketchbookList.jsx
+++ frontend/src/components/mydrawer/mysketchbook/MySketchbookList.jsx
@@ -14,20 +14,31 @@ import {
   ImgP,
 } from "components/mydrawer/MydrawerStyle";
 //mui아이콘 중 방향 버튼 아이콘을 가져
+import { Modal, Box } from "@mui/material/";
+import { Buttonthree } from "components/common/button/ButtonStyle";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import DrawSaved from "utils/canvas/DrawSaved";
+
+import diaryBackground from "assets/image/diaryBackground.svg";
 
 const MySketchbookList = () => {
+  // 모달 오픈 변수
+  const [open, setOpen] = useState(false);
+  const handleClose = () => setOpen(false);
+  // 내 스케치북 데이터
+  const [datas, setDatas] = useState([]);
+  // 스케치북 드로우 데이터(모달에 띄우는)
+  const [sketchDraw, setSketchDraw] = useState([]);
+
   // axios로 내 스케치북 데이터 불러오기
-
-  const userSeq = useSelector((state) => state.user);
-
+  const userSeq = useSelector((state) => state.user.accessToken);
   useEffect(() => {
     const sketchBooks = async () => {
       await axios
         .get("https://j8b201.p.ssafy.io/api/members/sketch/list", {
           headers: {
-            Authorization: userSeq.accessToken,
+            Authorization: userSeq,
           },
         })
         .then((response) => {
@@ -40,6 +51,25 @@ const MySketchbookList = () => {
     };
     sketchBooks();
   }, []);
+
+  // 스케치북 클릭했을 때 axios로 값을 불러옴
+  const handleClick = (id) => {
+    axios
+      .get(`https://j8b201.p.ssafy.io/api/members/sketch/${id}`, {
+        headers: {
+          Authorization: userSeq,
+        },
+      })
+      .then((response) => {
+        console.log(response.data.content.sketchTraceData);
+
+        setSketchDraw(response.data.content.sketchTraceData);
+        setOpen(true);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   // 이전 버튼 스타일
   const SamplePrevArrow = (props) => {
@@ -89,11 +119,20 @@ const MySketchbookList = () => {
     nextArrow: <SampleNextArrow />,
   };
 
-  // 내 스케치북 데이터
-  const [datas, setDatas] = useState([]);
-
-  // 스케치북 날짜를 한국 시간으로 바꿈
-  const [dateString, setDateString] = useState("");
+  // 모달 mui 스타일
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    border: "1px solid #ff8f5c",
+    boxShadow: 24,
+    outline: "none",
+    p: 4,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  };
 
   return (
     <Container>
@@ -101,8 +140,18 @@ const MySketchbookList = () => {
       {datas.length > 0 ? (
         <Slider {...settings}>
           {datas.map((data) => (
-            <MydraweritemContainer>
-              <MySketchbookimage src={data.sketchImgUrl} />
+            <MydraweritemContainer key={data.sketchId}>
+              <MySketchbookimage
+                src={data.sketchImgUrl}
+                bgimg={diaryBackground}
+                onClick={() => handleClick(data.sketchId)}
+              />
+              <Modal open={open} onClose={handleClose}>
+                <Box sx={style}>
+                  <DrawSaved sketchDraw={sketchDraw} />
+                  <Buttonthree onClick={handleClose}>확인</Buttonthree>
+                </Box>
+              </Modal>
               <ImgP>
                 {new Date(data.savedAt).toLocaleString("ko-KR", {
                   timeZone: "Asia/Seoul",
