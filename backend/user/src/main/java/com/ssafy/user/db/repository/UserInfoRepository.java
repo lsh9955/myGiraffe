43b52756dd857f6cc63cf52c7adfb4fa1eea9d3b
이석훈 <cqe9933@naver.com>
2023-04-05 11:14:30 +0900

    🚑 fix: develop merge 취소

--- backend/user/src/main/java/com/ssafy/user/db/repository/UserInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.user.db.repository;
-
-import com.ssafy.user.db.entity.UserInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserInfoRepository extends JpaRepository<UserInfo, String> {
-
-}
